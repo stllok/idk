20b9aabfffb5d39b663fa8a0524382de1eb3bfa9
lokson <osustllok@gmail.com>
2025-11-18 11:43:51 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,4 +3,4 @@
 git clone https://github.com/chenkenanalytic/handwritting_data_all.git --depth 1
 cat ./handwritting_data_all/all_data.zip* > ./handwritting_data_all/all_data.zip
 rm -rf cleaned_data/
-LC_ALL=C unzip ./handwritting_data_all/all_data.zip -d "."
+unzip -O big5 ./handwritting_data_all/all_data.zip -d "."
